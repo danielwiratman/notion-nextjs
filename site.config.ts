53e19bd243c0b230592e3e275b777230d03d91c4
Daniel Wiratman <74503671+danielwiratman@users.noreply.github.com>
2022-11-11 12:06:03 +0700

    changed domain

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Daniel Wiratman',
-  // domain: 'mica-principle-00b.notion.site',
+  domain: 'danielwiratman.vercel.app',
   author: 'Daniel Wiratman',
 
   // open graph metadata (optional)
